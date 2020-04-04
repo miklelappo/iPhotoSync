a4d12364a2e82f3d40e5d97d68561be2da872f54
Mikhail Lappo <miklelappo@gmail.com>
2020-04-04 19:52:49 +0200

    Add recursive album resolution

--- src/db.rs
+++ src/db.rs
@@ -1,4 +1,5 @@
 use rusqlite::{Connection, Result, NO_PARAMS};
+use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct Asset {
@@ -22,6 +23,7 @@ impl Clone for Asset {
 pub fn get_db_assets(filename: &str, mut ret: Vec<Asset>) -> Result<Vec<Asset>> {
     let filename = format!("{}/database/Photos.sqlite", filename);
     let conn = Connection::open(filename)?;
+    // Get all assets
     let mut backup_table_statement = conn.prepare("
         select ZGENERICALBUM.ZTITLE, ZDIRECTORY, ZFILENAME, ZORIGINALFILENAME
         from Z_26ASSETS 
@@ -36,9 +38,37 @@ pub fn get_db_assets(filename: &str, mut ret: Vec<Asset>) -> Result<Vec<Asset>> 
             filename: row.get(2)?,
             original_filename: row.get(3)?,
         })
-	})?;
+    })?;
+
+    // Extract iPhotoEvents album paths (folders)
+    let mut albums_statement = conn.prepare("
+        select a.Z_PK as \"PrivateKey\", a.ZTITLE as \"AlbumName\", b.ZTITLE as \"AlbumParent\" from ZGENERICALBUM a
+        inner join ZGENERICALBUM b on a.ZPARENTFOLDER = b.Z_PK where AlbumName NOTNULL
+    ")?;
+    let mut albums_hash: HashMap<String, String> = HashMap::new();
+    let mut albums_rows = albums_statement.query(NO_PARAMS)?;
+    while let Some(row) = albums_rows.next()?  {
+        albums_hash.insert(
+            row.get(1)?, //album name
+            match row.get(2) { //parent folder name if exists
+                Ok(name) => name,
+                _ => String::from(""),
+            }
+        );
+    }
+
     for asset in backup_table {
-		let asset = &asset?;
+        let asset = & mut asset?;
+        let mut album: String = asset.album_name.clone();
+        loop
+        {
+            let parent = albums_hash.get(&album).unwrap();
+            if parent.is_empty() {
+                break;
+            }
+            album = parent.to_string();
+            asset.album_name = format!("{}/{}", &album, asset.album_name);
+        }
         ret.push(asset.clone());
     }
     Ok(ret)
